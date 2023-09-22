78d8723dcdd994330f3f24d9f7703925ddd2bd3f
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-09-22 08:46:51 +0300

    Bundle css

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
