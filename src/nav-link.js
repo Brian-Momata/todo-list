de0505aadf6387c544d35cd511c06034e89c8af5
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-09-28 10:22:56 +0300

    Update the content container

--- src/nav-link.js
+++ src/nav-link.js
@@ -1,3 +1,29 @@
 export default function navLinkClickHandler(project) {
-  // Updates the content conatiner
+  const contentContainer = document.querySelector('.content');
+
+  // Clear the existing content in the container
+  contentContainer.innerHTML = '';
+
+  project.tasks.forEach((task, index) => {
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task-item');
+
+    const taskId = `task-${index}`;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = taskId;
+
+    const taskTitle = document.createElement('h3');
+    taskTitle.textContent = task.title;
+
+    const taskDescription = document.createElement('p');
+    taskDescription.textContent = task.description;
+
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTitle);
+    taskItem.appendChild(taskDescription);
+
+    contentContainer.appendChild(taskItem);
+  });
 }
